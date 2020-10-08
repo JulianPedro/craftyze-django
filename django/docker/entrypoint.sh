334277c41208e36bd23f461498bf9b14df84915b
Julian <julian.pedro@hotmail.com>
2020-10-07 21:53:55 -0400

    Adicionando ambiente

---
+++ django/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+      sleep 0.1
+    done
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate --fake-initial
+python manage.py collectstatic --noinput
+#python manage.py compress --force
+
+exec "$@"
