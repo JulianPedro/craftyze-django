ec0c11194c2fedc834623f2a3f352924afefb92c
Julian <julian.pedro@hotmail.com>
2020-10-11 15:19:53 -0400

    Add template itens

--- django/public/views.py
+++ django/public/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from company.models import Company
+from job.models import Job, Category
+
+
 class Home(TemplateView):
     template_name = "public/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['companies'] = Company.objects.all()
+        context['jobs'] = Job.objects.all()
+        context['categories'] = Category.objects.all()
+        return context
+
